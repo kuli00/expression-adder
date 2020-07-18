d37dd27f994e252fd40c82d0acb00cd6f8f711e5
Maciej Kulinski <kulinskimm@gmail.com>
2020-07-18 12:36:45 +0200

    add new equation function

--- src/components/Container/Container.styled.ts
+++ src/components/Container/Container.styled.ts
@@ -4,3 +4,21 @@ export const Wrapper = styled.div`
   width: 1080px;
   margin: 0 auto;
 `;
+
+export const Header = styled.h1`
+  font-size: 22px;
+  line-height: 26px;
+  letter-spacing: 0.12px;
+  font-weight: 600;
+  padding: 0;
+  margin: 15px 0;
+  text-align: center;
+`;
+
+export const P = styled.p`
+  font-size: 14px;
+  line-height: 17px;
+  letter-spacing: 0.2px;
+  text-align: center;
+  margin: 5px 0;
+`;
